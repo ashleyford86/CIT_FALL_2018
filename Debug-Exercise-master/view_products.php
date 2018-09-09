be0e68e30dd00fc481ac6fcbf9bcba7d5c1d0a2f
ashleyford86 <42522334+ashleyford86@users.noreply.github.com>
2018-09-09 17:41:31 -0400

    Add files via upload

--- Debug-Exercise-master/view_products.php
+++ Debug-Exercise-master/view_products.php
@@ -20,64 +20,67 @@
 <div id="wrapper">
 
 	<h2>Please select the quantity of items you would like to put into your Shopping Cart</h2>
-  
+
 
 	<div class="form-container">
 
 	<form action="<?php echo URL_ROOT . '/add_products_to_cart.php' ?>" method="post">
-    
+
     <?php
 		//create a loop that writes a form that displays all of the products in the SESSION inventory array
-		
+
 		$aryProductArray = unserialize($_SESSION['aryProductArray']);
-	
-	
-		for ($x = 0; $x < count($arryProductArray); $x++) {
+
+// took the r out of $arrayProductArray
+		for ($x = 0; $x < count($aryProductArray); $x++) {
 	?>
     <fieldset>
 		<legend>Product Details</legend>
-			
+
             <?php
 				//pull the next object out of the array
 				$thisProduceItem = $aryProductArray[$x];
 			?>
-            
+
+
             <div>
             	<b><?php echo $thisProduceItem->product_name ?></b><br />
                 Price: $<?php echo $thisProduceItem->product_price ?>
-                
+
                 <?php
 					switch ($thisProduceItem->price_type) {
 						case 1:
 							echo "/per pound";
-							break;	
+							break;
 						case 2:
 							echo "/each";
 							break;
-					
+
 				?>
 			</div>
-            
+
 			<div><label for="quantity<?php echo $x ?>">Quantity Requested</label> <input id="quantity<?php echo $x ?>" name="quantity<?php echo $x ?>" type="text" size="10" /></div>
 	</fieldset>
-    
-    <?php		
-			
-		}
-	?>
-	
-	
-    
-	<div class="buttonrow">
-		<input type="submit" value="Save" class="button" />
 
-		<input type="button" value="Discard" class="button" />
+    <?php
+
+		}
+		//added a } to close line 35
+	}
+	?>
+
+
+
+	<div class="buttonrow">
+		<input type="submit"  value="Save" class="button" />
+
+		<input type="button"  value="Discard" class="button" />
 	</div>
 
 	</form>
-	
+
 	</div><!-- /form-container -->
-	
+
 </div><!-- /wrapper -->
 
 
--- Debug-Exercise-master/webconfig.php
+++ Debug-Exercise-master/webconfig.php
@@ -1,4 +1,7 @@
 <?php
 
-define('ABSOLUTE_PATH', '');
-define('URL_ROOT', '');
+define('ABSOLUTE_PATH', '/home/asmote/htdocs/a1/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:23631/a1/Debug-Exercise-master/');
+
+//added a closing php tag and added the right path/url root
+?>
